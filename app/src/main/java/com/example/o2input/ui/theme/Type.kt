8045ff5b1b6d1fd954d0764318c06ea80fa0f1f9
Norbert445 <norbimacsali445@gmail.com>
2025-04-10 19:21:18 +0200

    feat: typography

--- app/src/main/java/com/example/o2input/ui/theme/Type.kt
+++ app/src/main/java/com/example/o2input/ui/theme/Type.kt
@@ -2,33 +2,38 @@ package com.example.o2input.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.o2input.R
 
-// Set of Material typography styles to start with
+val interFontFamily = FontFamily(
+    Font(R.font.inter_regular, FontWeight.Normal),
+    Font(R.font.inter_medium, FontWeight.Medium),
+    Font(R.font.inter_semi_bold, FontWeight.SemiBold),
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+    bodyMedium = TextStyle(
+        fontFamily = interFontFamily,
+        fontWeight = FontWeight.W400,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        lineHeight = 22.sp,
+        letterSpacing = 0.01.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = interFontFamily,
+        fontWeight = FontWeight.W500,
+        fontSize = 16.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.16.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = interFontFamily,
+        fontWeight = FontWeight.W600,
+        fontSize = 14.sp,
+        lineHeight = 17.sp,
+        letterSpacing = 0.16.sp
     )
-    */
 )
